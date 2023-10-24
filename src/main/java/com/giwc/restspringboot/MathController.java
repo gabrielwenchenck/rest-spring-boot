c990e6ef71c8153f55d550a61d2c327cd8a30486
Gabriel <wenck13@gmail.com>
2023-10-24 14:20:11 -0300

    sum method

--- src/main/java/com/giwc/restspringboot/MathController.java
+++ src/main/java/com/giwc/restspringboot/MathController.java
@@ -10,7 +10,22 @@ public class MathController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
-        return 1D;
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        return null;
+    }
+
+    private boolean isNumeric(String numberOne) {
+        return false;
     }
 }
