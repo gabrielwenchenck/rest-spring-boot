deb3b47524b33a605fec956ebbe171cb4aa763ee
Gabriel <wenck13@gmail.com>
2023-10-24 13:32:40 -0300

    MathController

---
+++ src/main/java/com/giwc/restspringboot/MathController.java
@@ -0,0 +1,16 @@
+package com.giwc.restspringboot;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+}
